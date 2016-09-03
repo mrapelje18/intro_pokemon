2f8ba6e127479f18f296f11af03fc3992134431a
mrapelje18 <mrapelje18@windwardschool.org>
2016-09-02 18:26:25 -0700

    did assignment for comments and errors
    Printed pokeball
    Learned about comments
    Found errors

--- src/intro_pokemon/Pokemon_learn.java
+++ src/intro_pokemon/Pokemon_learn.java
@@ -4,6 +4,6 @@ public class Pokemon_learn {
 public static void main (String[]args) {
 	System.out.println();
 	System.out.print("hi");
-	System.out.print("hello");
+	System.out.print("/*, but not \\*!");
 }
 }
